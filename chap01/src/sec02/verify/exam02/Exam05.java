b0a3067aa6c9b7c86cae03b79b053dad2d1dc47b
GoRyeoJin <sooon9254@naver.com>
2022-04-24 20:45:59 +0900

    for문과 while문

---
+++ chap01/src/sec02/verify/exam02/Exam05.java
@@ -0,0 +1,28 @@
+package sec02.verify.exam02;
+
+public class Exam05 {
+	
+	public static void main(String[] args) {
+		
+		/*
+			출력 형식
+			*
+			**
+			***
+			****
+		 */
+		
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<=i; j++) {
+				
+				System.out.print("*");
+				
+				if(j==i) {
+					System.out.println();
+				}
+				
+			}
+		}
+	}
+
+}
